74d7a57b017a9f32d261cb701adc835719a29c7a
Mosiah Rabello <100864562+mosiahrs@users.noreply.github.com>
2022-06-22 14:38:56 -0300

    Add files via upload
    First commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function calcular(){
+    let bebeu = window.document.getElementById('entrada')
+    let res = window.document.getElementById('saida')
+    let qtd = Number(bebeu.value)
+
+    res.innerHTML = `<p> Você bebeu <b>${qtd} litros</b> de água, Parabéns está se hidratando direitinho</p><br><p>Gabriel se orgulha de você</p>`
+
+    if(qtd < 3){
+        res.innerHTML = `<br><p>Você não está se hidratando direitinho!<br> É preciso pelo menos <strong> 3 lts</strong> de água por dia <br><br>Gabriel não vai ficar orgulhoso de você</p>`
+    }
+}
