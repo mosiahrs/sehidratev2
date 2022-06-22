04db5b46dc1f986e6559ea667e23bac42dd1140a
Mosiah Silva <100864562+mosiahrs@users.noreply.github.com>
2022-06-22 16:45:25 -0300

    Update script.js
    Alterando a quantidade mínima de agua

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ function calcular(){
     res.innerHTML = `<p> Você bebeu <b>${qtd} litros</b> de água, Parabéns está se hidratando direitinho</p><br><p>Gabriel se orgulha de você</p>`
 
     if(qtd < 3){
-        res.innerHTML = `<br><p>Você não está se hidratando direitinho!<br> É preciso pelo menos <strong> 3 lts</strong> de água por dia <br><br>Gabriel não vai ficar orgulhoso de você</p>`
+        res.innerHTML = `<br><p>Você não está se hidratando direitinho!<br> É preciso pelo menos <strong> 2 lts</strong> de água por dia <br><br>Gabriel não vai ficar orgulhoso de você</p>`
     }
 }
